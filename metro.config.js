92f67a39c7d6c66dfcf517eb78fb9515d6a8bd6c
Jamil Salomão <132591829+jamilsalomao@users.noreply.github.com>
2025-09-25 12:19:41 -0300

    Retira o nativewind

--- metro.config.js
+++ metro.config.js
@@ -1,6 +1,6 @@
+// metro.config.js - DEPOIS
 const { getDefaultConfig } = require("expo/metro-config");
-const { withNativeWind } = require('nativewind/metro');
  
-const config = getDefaultConfig(__dirname)
+const config = getDefaultConfig(__dirname);
  
-module.exports = withNativeWind(config, { input: './global.css' })
+module.exports = config;
